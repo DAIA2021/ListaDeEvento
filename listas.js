f277bdc307b5ee479a22d2ea7dd347f69468aca5
Daiana Oliveira <daiana.davi2411@gmail.com>
2021-12-07 14:09:43 -0300

    atividade 1 logica de programacao

---
+++ listas.js
@@ -0,0 +1,27 @@
+var datadoevento = [ "informe data do evento"] 
+var datadoevento = 4/12/2021
+if(datadoevento.length > 05/12/2021){
+    console.log("Evento permitido")
+} else {
+    console.log("Cadastro não permitido, data inválida")
+
+}
+ 
+var idade =[ "Informe idade do participante"]
+var idade = 10
+if (idade.length > 18){
+    console.log (" Permitido o cadastro")
+} else {
+    console.log ("Cadastro não permitido, idade inválida")
+}
+
+//Lista de participantes e palestrantes por evento
+var quantidadedeParticipante =["informe quantidade de Participantes"] 
+var quantidade =100
+if(quantidadedeParticipante.length < 100 ){
+    console.log ("Permitido cadastro")
+}else {
+    console.log ("Informamos que o número de participantes foi excedido")
+
+        
+}     
